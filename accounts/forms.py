b03a075c65389b2232e58d3bb69d9c95735caef9
우동진 <wjinh93@gmail.com>
2021-10-08 22:12:23 +0900

    created main-login page

--- accounts/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.contrib.auth.forms import User
-from .models import CustomUser
-
-
-class CustomUserCreationForm(forms.ModelForm):
-    class Meta:
-        model = CustomUser
-        fields ='__all__'
